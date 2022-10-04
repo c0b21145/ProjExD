f4b28588b3626bbc3ea64c3130ef33a6b29ac907
Miura <c0b21145fc@edu.teu.ac.jp>
2022-10-04 15:01:22 +0900

    4問め

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,29 +1,33 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+from turtle import right
 
-def button_click(event):
+def button_click(event):#３問目
     btn = event.widget
     txt = btn["text"]
     tkm.showinfo(txt,f"{txt}のボタンが押されました")
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x500")#１門目
 
-for i, num in enumerate(range(9,-1,-1),0):
+entry = tk.Entry(root,width=10,font=(", 40"),justify="right")#４問目
+entry.grid(row=0, column=0, columnspan=3)
+
+for i, num in enumerate(range(9,-1,-1),0):#２問目
     button = tk.Button(root,text=f"{num}",font=("",30),width=4,height=2)
     # button.pack()
     if num > 6:
         button.bind("<1>", button_click)
-        button.grid(row=0, column=i)
+        button.grid(row=1, column=i)
     elif num > 3:
         button.bind("<1>", button_click)
-        button.grid(row=1, column=i%3)
+        button.grid(row=2, column=i%3)
     elif num > 0:
         button.bind("<1>", button_click)
-        button.grid(row=2, column=i%3)
+        button.grid(row=3, column=i%3)
     else:
         button.bind("<1>", button_click)
-        button.grid(row=3, column=0)
+        button.grid(row=4, column=0)
 
 
 root.mainloop()
