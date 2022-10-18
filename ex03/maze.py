18f0f46e52fbdb7b625f3baa4addbc9aa687330f
Miura <c0b21145fc@edu.teu.ac.jp>
2022-10-18 17:09:10 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,8 @@
 from email.mime import image
 import tkinter as tk
+import tkinter.messagebox as tkm
 import maze_maker
+import random
 
 def key_down(event):
     global key
@@ -13,6 +15,7 @@ def key_up(event):
 def main_proc():
     global mx, my
     global cx, cy
+    # 迷路を移動する
     if key == "Up":
         my -= 1
     if key == "Down":
@@ -32,8 +35,42 @@ def main_proc():
             mx += 1
         if key == "Right":
             mx -= 1
+    if key == "q":
+        exit()
     canv.coords("tori",cx,cy)
     root.after(100,main_proc)
+
+def random_work():
+    global ex,ey
+    global ecx, ecy
+    work = random.randint(1,4)
+    if work%4==0:
+        ey -= 1
+    elif work%4==1:
+        ey += 1
+    elif work%4==2:
+        ex -= 1
+    else:
+        ex += 1
+    if maze_data[ey][ex] == 0:
+        ecx, ecy = ex*100+50, ey*100+50
+        if work%4==0:
+            ey += 1
+        elif work%4==1:
+            ey -= 1
+        elif work%4==2:
+            ex += 1
+        else:
+            ex -= 1
+    canv.coords("e_tori",ecx,ecy)
+    root.after(1000,random_work)
+
+            
+
+def exit():
+    check = tkm.askyesno(title="終わりにしますか？",message="止めるのならOKを押してください")
+    if check == True:
+        root.destroy()
 
 
 if __name__ == "__main__":
@@ -47,19 +84,28 @@ if __name__ == "__main__":
     maze_data = maze_maker.make_maze(15,9)
     maze_maker.show_maze(canv,maze_data) # 迷路を表示
 
-    tori = tk.PhotoImage(file="ex03/fig/3.png") # 画像の選択
+
+    tori_img = [f"ex03/fig/{i}.png" for i in range(10)] # こうかとんの画像のリスト
+    con = random.randint(0,9) # 画像変更時のフラグ
+    tori = tk.PhotoImage(file=tori_img[con])            
+    # tori = tk.PhotoImage(file="ex03/fig/3.png") # 画像の選択
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
     canv.create_image(cx,cy,image=tori,tag="tori") # こうかとんを表示
 
+    ex,ey = 4, 4
+    ecx, ecy = ex*100+50, ey*100+50
+    e_tori = tk.PhotoImage(file=tori_img[con])            
+    canv.create_image(cx,cy,image=e_tori,tag="e_tori") # こうかとんを表示
+
+
+
+
     key = "" # 現在押されているキーを表す変数
 
     root.bind("<KeyPress>",key_down)
-
     root.bind("<KeyRelease>",key_up)
-
     main_proc()
-
-
+    random_work()
 
     root.mainloop()
