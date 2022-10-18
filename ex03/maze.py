c1c56e45e11101e4cd8044008511202638bd783b
Miura <c0b21145fc@edu.teu.ac.jp>
2022-10-18 14:22:32 +0900

    rensyu4

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,3 +1,4 @@
+from email.mime import image
 import tkinter as tk
 
 if __name__ == "__main__":
@@ -7,5 +8,11 @@ if __name__ == "__main__":
 
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
+
+    tori = tk.PhotoImage(file="ex03/fig/3.png")
+    cx, cy = 300, 400
+    canv.create_image(cx,cy,image=tori,tag="tori")
+
+    key = "" # 現在押されているキーを表す変数
 
     root.mainloop()
