bf31bb6fb9e5357329f16c125d6c0245ead6e0ee
Miura <c0b21145fc@edu.teu.ac.jp>
2022-10-18 15:30:27 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,15 +11,27 @@ def key_up(event):
     key = ""
 
 def main_proc():
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    if maze_data[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
     canv.coords("tori",cx,cy)
     root.after(100,main_proc)
 
@@ -32,8 +44,13 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
 
+    maze_data = maze_maker.make_maze(15,9)
+    maze_maker.show_maze(canv,maze_data) # 迷路を表示
+
     tori = tk.PhotoImage(file="ex03/fig/3.png") # 画像の選択
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
+    canv.create_image(cx,cy,image=tori,tag="tori") # こうかとんを表示
 
     key = "" # 現在押されているキーを表す変数
 
@@ -43,9 +60,6 @@ if __name__ == "__main__":
 
     main_proc()
 
-    maze_data = maze_maker.make_maze(15,9)
-    maze_maker.show_maze(canv,maze_data) # 迷路を表示
-    canv.create_image(cx,cy,image=tori,tag="tori") # こうかとんを表示
 
 
     root.mainloop()
