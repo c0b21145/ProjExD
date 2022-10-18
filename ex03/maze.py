9a301c438ec2008813c5388ae404f87d801f106a
Miura <c0b21145fc@edu.teu.ac.jp>
2022-10-18 14:42:38 +0900

    rensyu7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,28 @@
 from email.mime import image
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx += 20
+    canv.coords("tori",cx,cy)
+    root.after(100,main_proc)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -14,5 +36,11 @@ if __name__ == "__main__":
     canv.create_image(cx,cy,image=tori,tag="tori")
 
     key = "" # 現在押されているキーを表す変数
+
+    root.bind("<KeyPress>",key_down)
+
+    root.bind("<KeyRelease>",key_up)
+
+    main_proc()
 
     root.mainloop()
