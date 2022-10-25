0f48810c4aff726d9fd3e1890bf96972fef40fd2
Miura <c0b21145fc@edu.teu.ac.jp>
2022-10-25 14:26:56 +0900

    rensyu3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,21 +9,23 @@ def main():
     bg_sfc = pg.image.load("ex04/fig/pg_bg.jpg") # 背景
     bg_rct = bg_sfc.get_rect()
 
+    tori_sfc = pg.image.load("ex04/fig/6.png") # surface
+    tori_sfc = pg.transform.rotozoom(tori_sfc,0, 2.0)
+    tori_rct = tori_sfc.get_rect() # rect
+    tori_rct.center = 900, 400
+
+
     clock = pg.time.Clock()
     while True:
 
-    # tori_sfc = pg.image.load("fig/6.png") # surface
-    # tori_rct = tori_sfc.get_rect() # rect
-    # tori_rct.center = 700, 400
-    # scrn_sfc.blit(tori_sfc, tori_rct) # スクリーンにこうかとんを貼り付ける
-
         scrn_sfc.blit(bg_sfc, bg_rct) # スクリーンに背景を貼り付ける
-        pg.display.update()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-        
-        
+
+        scrn_sfc.blit(tori_sfc, tori_rct) # スクリーンにこうかとんを貼り付ける        
+
+        pg.display.update()
         clock.tick(1000)
 
 
