a07e4bc7b46b59914b28395d86c658e6ce6f326f
Miura <c0b21145fc@edu.teu.ac.jp>
2022-10-25 15:46:09 +0900

    rensyu5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,12 +2,25 @@ import pygame as pg
 import sys
 from random import randint
 
+def check_bound(obj_rct, scr_rct):
+    """"
+    obj_rct こうかとんrct 又は 爆弾rct
+    scr_rct スクリーンrct
+    領域内：+1 領域外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: tate = -1
+    return yoko, tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("ex04/fig/pg_bg.jpg") # 背景
+    # bg_sfc = pg.image.load("../../Desktop/ガラクタ置き場/素材/背景/nc273656.png") # 背景
     bg_rct = bg_sfc.get_rect()
 
     tori_sfc = pg.image.load("ex04/fig/6.png") # surface
@@ -17,9 +30,11 @@ def main():
 
     bomb_sfc = pg.Surface((20,20))
     bomb_sfc.set_colorkey((0, 0, 0)) # 隅の黒いところを透明にする
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10) # 縁を書く
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10) # 円を書く
     bomb_rct = tori_sfc.get_rect()
     bomb_rct.centerx, bomb_rct.centery = randint(0, scrn_rct.width), randint(0, scrn_rct.height)
+    vx, vy = +1, +1
+
 
     clock = pg.time.Clock()
     while True:
@@ -35,9 +50,26 @@ def main():
         if key_states[pg.K_DOWN]: tori_rct.centery += 1 # こうかとんを下に移動
         if key_states[pg.K_LEFT]: tori_rct.centerx -= 1 # こうかとんを左に移動
         if key_states[pg.K_RIGHT]: tori_rct.centerx += 1 # こうかとんを右に移動
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+        if yoko == -1 :
+            if key_states[pg.K_LEFT]:tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:tori_rct.centerx -= 1
+        if tate == -1 :
+            if key_states[pg.K_UP]:tori_rct.centery += 1
+            if key_states[pg.K_DOWN]:tori_rct.centery -= 1
+            
+        yoko, tate = check_bound(bomb_rct,scrn_rct)
+        vx *= yoko
+        vy *= tate
 
-        scrn_sfc.blit(bomb_sfc, bomb_rct)
+        bomb_rct.move_ip(vx, vy)
+        scrn_sfc.blit(bomb_sfc, bomb_rct) # スクリーンに爆弾を貼り付ける
 
+        
+
+        if tori_rct.colliderect(bomb_rct): # ゲームオーバー判定
+            return
+        
         pg.display.update()
         clock.tick(1000)
 
